c7a6434130f65c65733d40632a114718042b0b49
TinyYixin1129 <TheDearest1129@gmail.com>
2024-03-29 15:17:33 +0100

    Update

--- src/main/java/full/stack/chatter/model/ChatRoom.java
+++ src/main/java/full/stack/chatter/model/ChatRoom.java
@@ -1,4 +1,47 @@
 package full.stack.chatter.model;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class ChatRoom {
+    private long id;
+    private String title;
+    private String description;
+    private User creator;
+    private List<User> user_list;
+    private int duration;
+    private LocalDateTime createDate;
+    private LocalDateTime expireDate;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public List<User> getUser_list() {
+        return user_list;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public LocalDateTime getExpireDate() {
+        return expireDate;
+    }
 }
