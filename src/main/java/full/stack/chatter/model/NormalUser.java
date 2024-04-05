90824f39c8a7e5fb48f6f95e22912209401cfe5e
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2024-04-06 00:15:25 +0200

    add descriptors for base class

--- src/main/java/full/stack/chatter/model/NormalUser.java
+++ src/main/java/full/stack/chatter/model/NormalUser.java
@@ -1,5 +1,10 @@
 package full.stack.chatter.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "NormalUsers")
 public class NormalUser extends User{
     public NormalUser(){
         super();
