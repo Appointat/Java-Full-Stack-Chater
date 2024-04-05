6800fa35e31337888d22f93f5cc14f25597bb60d
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2024-04-06 01:10:32 +0200

    feat: add apis for the admin users

--- src/main/java/full/stack/chatter/services/ServicesRequest.java
+++ src/main/java/full/stack/chatter/services/ServicesRequest.java
@@ -1,6 +1,7 @@
 package full.stack.chatter.services;
 
 
+import full.stack.chatter.model.AdminUser;
 import full.stack.chatter.model.NormalUser;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -17,20 +18,26 @@ public class ServicesRequest {
     @PersistenceContext
     EntityManager em;
 
-    public void addUser(NormalUser normal_user) {
+    /*
+    APIs for NormalUser
+     */
+    public void addNormalUser(NormalUser normal_user) {
         em.persist(normal_user);
     }
 
     public void updateUser(NormalUser user) {
+        // TODO
         em.merge(user);
     }
 
     public NormalUser getOneUser(int id) {
+        // TODO
         //return un user via la clé primaire
         return em.find(NormalUser.class, id);
     }
 
     public void deleteOneUser(int id) {
+        // TODO
         //return un user via la clé primaire
         em.remove(em.find(NormalUser.class, id));
     }
@@ -40,5 +47,20 @@ public class ServicesRequest {
         return q.getResultList();
     }
 
+    /*
+    APIs for AdminUser
+     */
+    public void addAdminUser(AdminUser admin_user) {
+        em.persist(admin_user);
+    }
 
+    public void deleteAdminUser(NormalUser normal_user) {
+        // TODO
+        em.remove(normal_user);
+    }
+
+    public List<AdminUser> getAdminUsers() {
+        Query q = em.createQuery("select nu from AdminUser nu");
+        return q.getResultList();
+    }
 }
