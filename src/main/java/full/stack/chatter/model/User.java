d156671dab54b8180e9acfe10e153c7e472f1f3b
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2024-04-06 00:39:57 +0200

    refactor: rename the vars created_chat_rooms, invited_chat_rooms

--- src/main/java/full/stack/chatter/model/User.java
+++ src/main/java/full/stack/chatter/model/User.java
@@ -2,6 +2,9 @@ package full.stack.chatter.model;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @MappedSuperclass
 public class User {
@@ -22,24 +25,16 @@ public class User {
     @Column(name = "password")
     private String password;
 
-//    @Transient
-//    private List<Long> createdChatRoom;
+    @Transient
+    private List<Long> created_chat_rooms = new ArrayList<>();
 
-//    @Transient
-//    private List<Long> invitedChatRoom;
+    @Transient
+    private List<Long> invited_chat_rooms = new ArrayList<>();
 
     @Column(name = "is_active")
     private Boolean is_active;
 
     public User() {
-    }
-
-    public User(String first_name, String last_name, String email, String password, Boolean is_active) {
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.email = email;
-        this.password = password;
-        this.is_active = is_active;
     }
 
     public Long getId() {
@@ -54,13 +49,13 @@ public class User {
         return password;
     }
 
-//    public List<Long> getCreatedChatRoom() {
-//        return !this.createdChatRoom.isEmpty() ? createdChatRoom : null;
-//    }
+    public List<Long> getCreatedChatRoom() {
+        return !this.created_chat_rooms.isEmpty() ? this.created_chat_rooms : null;
+    }
 
-//    public List<Long> getInvitedChatRoom() {
-//        return !this.invitedChatRoom.isEmpty() ? invitedChatRoom : null;
-//    }
+    public List<Long> getInvitedChatRoom() {
+        return !this.invited_chat_rooms.isEmpty() ? this.invited_chat_rooms : null;
+    }
 
     public String getFirstName() {
         return this.first_name;
