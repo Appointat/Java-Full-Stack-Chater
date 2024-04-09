3ec2a4968c576063c74d0e3b2019a15565db8bf7
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2024-04-09 23:30:33 +0200

    feat: init the websocket controller

--- src/main/java/full/stack/chatter/controller/WebSocketController.java
+++ src/main/java/full/stack/chatter/controller/WebSocketController.java
@@ -1,5 +1,19 @@
 package full.stack.chatter.controller;
 
+import full.stack.chatter.model.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class WebSocketController {
+    // Handle messages sent to "/app/send/message",
+    // and broadcast the message to all clients subscribed to "/topic/messages"
+    @MessageMapping("/send/message")
+    @SendTo("/topic/messages")
+    public Message sendMessage(Message message) {
+        // TODO: Save message to database
+        return message; // Return message will be broadcast to all clients subscribed to "/topic/messages"
+    }
 
 }
