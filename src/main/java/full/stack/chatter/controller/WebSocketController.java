0318b51b2585034604008a6969ad0048e1107602
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2024-04-10 09:19:13 +0200

    feat: modify the controller

--- src/main/java/full/stack/chatter/controller/WebSocketController.java
+++ src/main/java/full/stack/chatter/controller/WebSocketController.java
@@ -1,18 +1,24 @@
 package full.stack.chatter.controller;
 
 import full.stack.chatter.model.Message;
+import full.stack.chatter.services.MessageManagementRequest;
+import jakarta.annotation.Resource;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WebSocketController {
+    @Resource
+    private MessageManagementRequest messageManagementRequest;
+
     // Handle messages sent to "/app/send/message",
     // and broadcast the message to all clients subscribed to "/topic/messages"
     @MessageMapping("/send/message")
     @SendTo("/topic/messages")
     public Message sendMessage(Message message) {
-        // TODO: Save message to database
+        // Save message to database
+        messageManagementRequest.addMessage(message);
         return message; // Return message will be broadcast to all clients subscribed to "/topic/messages"
     }
 
