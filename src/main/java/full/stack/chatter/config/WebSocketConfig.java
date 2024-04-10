b2533cf937b49cf97911df5f72140a0e8f8dea2d
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2024-04-10 09:19:36 +0200

    feat: add webSocketStompClient

--- src/main/java/full/stack/chatter/config/WebSocketConfig.java
+++ src/main/java/full/stack/chatter/config/WebSocketConfig.java
@@ -1,14 +1,34 @@
 package full.stack.chatter.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+
+import java.util.Collections;
+
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Bean
+    public WebSocketStompClient webSocketStompClient() {
+        WebSocketClient webSocketClient = new SockJsClient(
+                Collections.singletonList(new WebSocketTransport(new StandardWebSocketClient())));
+        WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+        return stompClient;
+    }
+
+
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
