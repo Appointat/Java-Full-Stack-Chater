6e499a12453e94c9bc710bf1f8c37deaea0e7ce2
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2024-05-23 00:26:11 +0200

    fix: fix the date init

--- src/main/java/full/stack/chatter/websocket/WebSocketConfig.java
+++ src/main/java/full/stack/chatter/websocket/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package full.stack.chatter.websocket;
 
 import full.stack.chatter.model.ChatRoom;
+import full.stack.chatter.model.NormalUser;
 import full.stack.chatter.services.UserAndRoomManagementRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +9,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-import java.util.List;
+import java.time.LocalDateTime;
 
 
 @Configuration
@@ -20,17 +21,21 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         // Establish the websocket for each existing chat room
-        List<ChatRoom> chat_rooms = userAndRoomManagementRequest.getChatRooms();
-        for (ChatRoom chat_room : chat_rooms) {
-            long id = chat_room.getId();
-            registry.addHandler(new WebSocketHandler(chat_room), "/chatroom/" + id).setAllowedOrigins("*");
-        }
+//        List<ChatRoom> chat_rooms = userAndRoomManagementRequest.getChatRooms();
+//        for (ChatRoom chat_room : chat_rooms) {
+//            long id = chat_room.getId();
+//            registry.addHandler(new WebSocketHandler(chat_room), "/chatroom/" + id).setAllowedOrigins("*");
+//        }
 
         // Test the websocket and the html
         ChatRoom chat_room = new ChatRoom();
         String title_test = "api_test_chat_room";
         long id_test = 100;
-        chat_room.setChatRoom(title_test, "This is a test chat room", null, null, null);
+        NormalUser normal_user = new NormalUser();
+        normal_user.setUser("test_normal_user", "Adam", "Smith", "123456", true);
+        LocalDateTime create_date = LocalDateTime.now();
+        LocalDateTime expire_date = create_date.plus(1, java.time.temporal.ChronoUnit.DAYS);
+        chat_room.setChatRoom(title_test, "This is a test chat room", normal_user, create_date, expire_date);
         registry.addHandler(new WebSocketHandler(chat_room), "/chatroom/" + id_test).setAllowedOrigins("*");
     }
 }
