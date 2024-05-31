ed480d88dbe5270aaf6f414ce5a8da2828bb7efc
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2024-05-31 14:51:49 +0200

    chore: format

--- src/main/java/full/stack/chatter/Application.java
+++ src/main/java/full/stack/chatter/Application.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @SpringBootApplication
 @EntityScan(basePackages = "full.stack.chatter")
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
