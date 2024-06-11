55463b56b98d206df06504bfe593cb363f900c64
Zikang CHEN <65004114+Appointat@users.noreply.github.com>
2024-06-12 00:03:24 +0200

    chore: reformat

--- src/main/java/full/stack/chatter/model/NormalUser.java
+++ src/main/java/full/stack/chatter/model/NormalUser.java
@@ -7,18 +7,19 @@ import java.util.List;
 
 @Entity
 @Table(name = "NormalUsers")
-public class NormalUser extends User{
-    public NormalUser(){
+public class NormalUser extends User {
+    public NormalUser() {
         super();
     }
-    public NormalUser(String first_name, String last_name, String email, String password){
+
+    public NormalUser(String first_name, String last_name, String email, String password) {
         this.first_name = first_name;
         this.last_name = last_name;
         this.email = email;
         this.password = password;
         this.is_active = true;
-        this.failed_attempt=0;
-        this.is_new=false;
+        this.failed_attempt = 0;
+        this.is_new = false;
     }
 
     @ElementCollection
